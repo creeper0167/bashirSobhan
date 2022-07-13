de0c0e75f524de616ca11aeb3e72f18dd9fce04b
Amin Arjang <m.amin.arjang@gmail.com>
2022-07-14 03:24:28 +0430

    initialize git

---
+++ src/test/java/com/bashir/demo/BashirSobhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bashir.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BashirSobhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
