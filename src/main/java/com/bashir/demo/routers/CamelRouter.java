2be79351cb0964e3fd9f4ea310fc165437b24bf9
Amin Arjang <m.amin.arjang@gmail.com>
2022-07-14 14:26:30 +0430

    add camelRouter.class

--- src/main/java/com/bashir/demo/routers/CamelRouter.java
+++ src/main/java/com/bashir/demo/routers/CamelRouter.java
@@ -1,5 +1,12 @@
 package com.bashir.demo.routers;
 
-public class CamelRouter {
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
+public class CamelRouter extends RouteBuilder{
+	@Override
+	public void configure() {
+		from("timer:producerTimer?period=50000").bean("CamelController?method=sendMessage").log("producer timer trriger");
+	}
 }
